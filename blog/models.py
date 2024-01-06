1171301192cb30d6dffa6c2c5287694dec829691
jveer634 <jaysparx9@gmail.com>
2024-01-07 00:19:05 +0530

    add: blog model, migrate ad register

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering=["-created_at", "-updated_at"]
+
+    def __str__(self) -> str:
+        return self.title
