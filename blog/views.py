2df34a97173b68c191c502aaa42cba8169a32865
jveer634 <jaysparx9@gmail.com>
2024-01-07 00:20:25 +0530

    chore: add blog list and detail view, templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from django.views import generic
+
+from  .models import Blog
+
 # Create your views here.
+
+
+class BlogList(generic.ListView):
+    model = Blog
+    template_name="blog_list.html"
+    # queryset = Blog.objects.all()
+
+
+class BlogDetail(generic.DetailView):
+    model = Blog
+    template_name="blog_detail.html"
+    # queryset = Blog.objects.all()
