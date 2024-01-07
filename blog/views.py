874f646a3790388fee015f17f8824fb94988a872
jveer634 <jaysparx9@gmail.com>
2024-01-07 19:39:12 +0530

    chore: add blog create view

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import generic
 
 from  .models import Blog
+from .forms import BlogForm
 
 # Create your views here.
 
@@ -17,3 +18,11 @@ class BlogDetail(generic.DetailView):
     model = Blog
     template_name="blog_detail.html"
     # queryset = Blog.objects.all()
+
+
+class BlogCreate(generic.CreateView):
+    model = Blog
+    form_class = BlogForm
+    template_name="blog_create.html"
+
+
